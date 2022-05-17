0d4ed2b0fe8ff0ddaf25dbb423f2d6275b66f7cf
alan zou <alan.zou@gmail.com>
2022-05-16 21:33:43 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8800;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
